f1b14edf3d485e9d216b43a5f051e9ec610e2a6e
Sandile <sandilembatha89@gmail.com>
2025-09-18 04:55:10 +0200

    Update images: - Use PNG - Update data.sql

--- src/main/java/co/za/vendingmachineapi/controller/PaymentController.java
+++ src/main/java/co/za/vendingmachineapi/controller/PaymentController.java
@@ -4,9 +4,11 @@ import co.za.vendingmachineapi.model.PurchaseRequest;
 import co.za.vendingmachineapi.service.PaymentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 public class PaymentController {
 
--- src/main/java/co/za/vendingmachineapi/controller/ProductController.java
+++ src/main/java/co/za/vendingmachineapi/controller/ProductController.java
@@ -5,12 +5,14 @@ import co.za.vendingmachineapi.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 public class ProductController {
 
